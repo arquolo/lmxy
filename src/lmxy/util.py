2c7166d74d48fd8135f25dad1b4a793391e4b714
Paul Maevskikh <arquolo@gmail.com>
2025-09-05 01:21:40 +0500

    FEAT aretry - support urllib, requests, httpx, aiohttp

--- src/lmxy/util.py
+++ src/lmxy/util.py
@@ -4,22 +4,44 @@ __all__ = [
     'warn_immediate_errors',
 ]
 
+import asyncio
 import logging
+import urllib.error
 from collections.abc import Callable
+from contextlib import suppress
 from functools import update_wrapper
 from inspect import iscoroutinefunction
+from types import CodeType
 from typing import Protocol, cast
 
-from httpx import HTTPError
 from tenacity import (
-    BaseRetrying,
     RetryCallState,
-    RetryError,
     retry,
     retry_if_exception_type,
     stop_after_attempt,
     wait_fixed,
 )
+
+_retriable_errors: tuple[type[BaseException], ...] = (
+    asyncio.TimeoutError,
+    urllib.error.HTTPError,
+)
+
+with suppress(ImportError):
+    import requests
+
+    _retriable_errors += (requests.HTTPError,)
+
+with suppress(ImportError):
+    import httpx
+
+    _retriable_errors += (httpx.HTTPError,)
+
+with suppress(ImportError):
+    import aiohttp
+
+    _retriable_errors += (aiohttp.ClientError,)
+
 
 logger = logging.getLogger(__name__)
 _ENV_VARS_TRUE_VALUES = frozenset({'1', 'ON', 'YES', 'TRUE'})
@@ -39,49 +61,64 @@ def aretry(
     *extra_errors: type[BaseException],
     max_attempts: int = 10,
     wait: float = 1,
+    override_defaults: bool = False,
 ) -> _Decorator:
     """Wrap sync or async function with a new `Retrying` object.
 
-    By default handles only HTTPError. To add more add more.
+    By default retries only if:
+    - asyncio.TimeoutError
+    - urllib.error.HTTPError
+    - requests.HTTPError
+    - httpx.HTTPError
+    - aiohttp.ClientError
+
+    To add more add more.
+    To disable default errors set `override_defaults`.
     """
     # Protect to not accidentally call aretry(fn)
     assert all(
         isinstance(tp, type) and issubclass(tp, BaseException)
         for tp in extra_errors
     )
+    retry_ = retry(
+        stop=stop_after_attempt(max_attempts),
+        wait=wait_fixed(wait),
+        retry=retry_if_exception_type(
+            (() if override_defaults else _retriable_errors) + extra_errors
+        ),
+        before_sleep=warn_immediate_errors,
+        reraise=True,
+    )
 
     def deco[**P, R](f: Callable[P, R]) -> Callable[P, R]:
-        w = retry(
-            stop=stop_after_attempt(max_attempts),
-            wait=wait_fixed(wait),
-            retry=retry_if_exception_type((HTTPError, *extra_errors)),
-            before_sleep=warn_immediate_errors,
-        )(f)
-        r: BaseRetrying = w.retry  # type: ignore[attr-defined]
+        wrapped_f = retry_(f)
 
         async def async_wrapper(*args: P.args, **kwargs: P.kwargs):
-            assert iscoroutinefunction(f)
-            copy = r.copy()
             try:
-                return await copy(f, *args, **kwargs)
-            except RetryError as e:
-                exc = e.last_attempt.exception()
-                assert exc is not None
-                raise exc from None
+                return await wrapped_f(*args, **kwargs)  # type: ignore[misc]
+            except BaseException as exc:
+                _declutter_tb(exc, f.__code__)
+                raise
 
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            copy = r.copy()
             try:
-                return copy(f, *args, **kwargs)
-            except RetryError as e:
-                exc = e.last_attempt.exception()
-                assert exc is not None
-                raise exc from None
+                return wrapped_f(*args, **kwargs)
+            except BaseException as exc:
+                _declutter_tb(exc, f.__code__)
+                raise
 
         w2 = async_wrapper if iscoroutinefunction(f) else wrapper
         return cast('Callable[P, R]', update_wrapper(w2, f))
 
     return deco
+
+
+def _declutter_tb(e: BaseException, code: CodeType) -> None:
+    tb = e.__traceback__
+    # Drop outer to `code` frames
+    while tb and tb.tb_frame.f_code is not code:
+        tb = tb.tb_next
+    e.__traceback__ = tb
 
 
 def warn_immediate_errors(s: RetryCallState) -> None:
@@ -102,7 +139,7 @@ def warn_immediate_errors(s: RetryCallState) -> None:
         qualname = (f'{mod}.{name}' if mod else name) if name else repr(fn)
 
     logger.warning(
-        'Retrying %s in %d seconds as it raised %s: %s.',
+        'Retrying %s in %.2g seconds as it raised %s: %s.',
         qualname,
         s.next_action.sleep,
         ex.__class__.__name__,
