630748e0164455ebba8d74ff630f8d081856cd6b
Paul M <arquolo@gmail.com>
2026-02-20 22:46:23 +0500

    FEAT aiohttp transport for httpx

--- src/lmxy/util.py
+++ src/lmxy/util.py
@@ -1,4 +1,5 @@
 __all__ = [
+    'AiohttpTransport',
     'aclient',
     'aretry',
     'client',
@@ -12,17 +13,21 @@ import asyncio
 import random
 import sys
 import urllib.error
-from collections.abc import Callable
+from contextlib import contextmanager
+from collections.abc import AsyncIterator, Callable, Iterator
 from datetime import timedelta
 from functools import update_wrapper
 from inspect import iscoroutinefunction
 from types import CodeType, FrameType
 from typing import Any, cast
+from urllib.parse import unquote
 
+import aiohttp
 import httpx
 from tenacity import RetryCallState, retry
 from glow import memoize, register_post_import_hook
 from loguru import logger
+from yarl import URL
 
 from ._env import env
 
@@ -34,14 +39,11 @@ _retriable_errors: list[type[BaseException]] = [
     TimeoutError,
     urllib.error.HTTPError,
     httpx.HTTPError,
+    aiohttp.ClientError,
 ]
 register_post_import_hook(
     lambda mod: _retriable_errors.append(mod.HTTPError),
     'requests',
-)
-register_post_import_hook(
-    lambda mod: _retriable_errors.append(mod.ClientError),
-    'aiohttp',
 )
 _inf = float('inf')
 
@@ -222,11 +224,217 @@ def guess_name(obj: object) -> str:
     return (f'{mod}.{name}' if mod else name) if name else repr(obj)
 
 
+# ---------------------------- aiohttp transport -----------------------------
+
+
+class AiohttpTransport(httpx.AsyncBaseTransport):
+    __slots__ = ('_get_session', '_session')
+
+    def __init__(
+        self,
+        *,
+        headers: dict[str, str] | None = None,
+        no_cookie: bool = True,
+        verify: bool = False,
+        keepalive_timeout: float = 15,
+        max_connections: int = 100,
+        max_connections_per_host: int = 0,
+        proxy: str | None = None,
+        retries: int = 0,
+        force_close: bool = False,
+    ) -> None:
+        def get_session() -> aiohttp.ClientSession:
+            connector = aiohttp.TCPConnector(
+                ssl=verify,
+                ttl_dns_cache=None,
+                keepalive_timeout=None if force_close else keepalive_timeout,
+                force_close=force_close,
+                limit=max_connections,
+                limit_per_host=max_connections_per_host,
+                enable_cleanup_closed=True,
+            )
+            return aiohttp.ClientSession(
+                connector=connector,
+                headers=headers,
+                proxy=proxy,
+                cookie_jar=aiohttp.DummyCookieJar() if no_cookie else None,
+                middlewares=[_RetryMiddleware(retries)] if retries else (),
+            )
+
+        # TCPConnector and ClientSession want running event loop in __init__.
+        # Construct them in async method so they will get it.
+        self._get_session = get_session
+        self._session: aiohttp.ClientSession | None = None
+
+    async def handle_async_request(
+        self, request: httpx.Request
+    ) -> httpx.Response:
+        if self._session is None:
+            self._session = self._get_session()
+        if self._session.closed:
+            raise RuntimeError('Transport is closed')
+
+        url = _httpx_to_yarl_url(request.url)
+        url_ = url.with_query(())
+        params = [(k, unquote(v)) for k, v in url.query.items()]
+
+        timeout = request.extensions.get('timeout', {})
+        sni_hostname = request.extensions.get('sni_hostname')
+
+        with _map_aiohttp_exceptions():
+            data: bytes | httpx.AsyncByteStream | None
+            try:
+                data = request.content or None
+            except httpx.RequestNotRead:
+                data = request.stream  # type: ignore
+                request.headers.pop('transfer-encoding', None)
+
+            rsp = await self._session.request(
+                method=request.method,
+                url=url_,
+                params=params or None,
+                data=data,
+                headers=request.headers,
+                allow_redirects=True,
+                auto_decompress=False,
+                compress=False,
+                skip_auto_headers=_SKIP_AUTO_HEADERS,
+                timeout=aiohttp.ClientTimeout(
+                    sock_connect=timeout.get('connect'),
+                    sock_read=timeout.get('read'),
+                    connect=timeout.get('pool'),
+                ),
+                server_hostname=sni_hostname,
+            ).__aenter__()
+
+        extensions = {'http_version': b'HTTP/1.1'}
+        if rsp.reason:
+            extensions['reason_phrase'] = rsp.reason.encode()
+
+        return httpx.Response(
+            rsp.status,
+            headers=rsp.raw_headers,
+            content=_AiohttpResponseStream(rsp),
+            request=request,
+            extensions=extensions,
+        )
+
+    async def aclose(self) -> None:
+        if self._session is not None:
+            await self._session.close()
+
+
+class _RetryMiddleware:
+    def __init__(self, attempts: int = 0) -> None:
+        self.attempts = attempts
+
+    async def __call__(
+        self, req: aiohttp.ClientRequest, handler: aiohttp.ClientHandlerType
+    ) -> aiohttp.ClientResponse:
+        rsp = await handler(req)
+        if rsp.status not in _HTTP_RETRY_CODES:
+            return rsp
+
+        for _ in range(self.attempts):  # Try more
+            rsp = await handler(req)
+            if rsp.status not in _HTTP_RETRY_CODES:
+                return rsp
+
+        return rsp
+
+
+class _AiohttpResponseStream(httpx.AsyncByteStream):
+    CHUNK_SIZE = 1024 * 16
+
+    def __init__(self, rsp: aiohttp.ClientResponse) -> None:
+        self._rsp = rsp
+
+    async def __aiter__(self) -> AsyncIterator[bytes]:
+        with _map_aiohttp_exceptions():
+            async for chunk in self._rsp.content.iter_chunked(self.CHUNK_SIZE):
+                yield chunk
+
+    async def aclose(self) -> None:
+        with _map_aiohttp_exceptions():
+            await self._rsp.__aexit__(None, None, None)
+
+
+def _httpx_to_yarl_url(url: httpx.URL) -> URL:
+    return URL.build(
+        scheme=url.scheme,
+        user=url.username or None,
+        password=url.password or None,
+        host=url.host,
+        port=url.port,
+        path=url.path,
+        query_string=url.query.decode(),
+        fragment=url.fragment,
+    )
+
+
+@contextmanager
+def _map_aiohttp_exceptions() -> Iterator[None]:
+    try:
+        yield
+    except Exception as exc:
+        for aiohttp_exc, httpx_exc in _AIOHTTP_TO_HTTPX_EXCEPTIONS.items():
+            if isinstance(exc, aiohttp_exc):
+                raise httpx_exc(str(exc)) from exc
+
+        if isinstance(exc, asyncio.TimeoutError):
+            raise httpx.TimeoutException(str(exc)) from exc
+
+        raise httpx.HTTPError(f'Unknown error: {exc!s}') from exc
+
+
+_SKIP_AUTO_HEADERS = frozenset(
+    {
+        'accept',
+        'accept-encoding',
+        'connection',
+        'content-encoding',
+        'deflate',
+        'user-agent',
+    }
+)
+_HTTP_RETRY_CODES = (
+    408,  # request timeout
+    429,  # too many requests
+    500,  # internal server error
+    502,  # bad gateway
+    503,  # service unavailable
+    504,  # gateway timeout
+)
+_AIOHTTP_TO_HTTPX_EXCEPTIONS: dict[type[Exception], type[Exception]] = {
+    # Order matters here, most specific exception first
+    aiohttp.ClientSSLError: httpx.ProtocolError,
+    aiohttp.ClientProxyConnectionError: httpx.ProxyError,
+    aiohttp.ClientConnectorDNSError: httpx.ConnectError,
+    aiohttp.ClientConnectorError: httpx.ConnectError,
+    aiohttp.SocketTimeoutError: httpx.ReadTimeout,
+    aiohttp.ServerTimeoutError: httpx.TimeoutException,
+    aiohttp.ServerDisconnectedError: httpx.ReadError,
+    aiohttp.ServerFingerprintMismatch: httpx.ProtocolError,
+    aiohttp.TooManyRedirects: httpx.TooManyRedirects,
+    aiohttp.ContentTypeError: httpx.ReadError,
+    aiohttp.ClientHttpProxyError: httpx.ProxyError,
+    aiohttp.ClientOSError: httpx.ConnectError,
+    aiohttp.ClientConnectionResetError: httpx.ConnectError,
+    aiohttp.ClientConnectionError: httpx.NetworkError,
+    aiohttp.ClientPayloadError: httpx.ReadError,
+    aiohttp.NonHttpUrlClientError: httpx.UnsupportedProtocol,
+    aiohttp.InvalidUrlClientError: httpx.UnsupportedProtocol,
+    aiohttp.InvalidURL: httpx.InvalidURL,
+    aiohttp.ClientError: httpx.RequestError,
+}
+
 # ------------------------------ httpx clients -------------------------------
 
 
 @memoize()  # Global pool for all HTTP requests
-def _get_transports() -> tuple[httpx.BaseTransport, httpx.AsyncBaseTransport]:
+def _get_transports(
+    v2: bool = False,
+) -> tuple[httpx.BaseTransport, httpx.AsyncBaseTransport]:
     limits = httpx.Limits(
         max_connections=env.MAX_CONNECTIONS,
         max_keepalive_connections=env.MAX_KEEP_ALIVE_CONNECTIONS,
@@ -239,11 +447,19 @@ def _get_transports() -> tuple[httpx.BaseTransport, httpx.AsyncBaseTransport]:
         limits=limits,
         retries=env.RETRIES,
     )
-    async_ = httpx.AsyncHTTPTransport(
-        verify=env.SSL_VERIFY,
-        limits=limits,
-        retries=env.RETRIES,
-    )
+    if v2:
+        async_ = AiohttpTransport(
+            verify=env.SSL_VERIFY,
+            max_connections=env.MAX_CONNECTIONS,
+            keepalive_timeout=env.KEEP_ALIVE_TIMEOUT,
+            retries=env.RETRIES,
+        )
+    else:
+        async_ = httpx.AsyncHTTPTransport(
+            verify=env.SSL_VERIFY,
+            limits=limits,
+            retries=env.RETRIES,
+        )
     return sync, async_
 
 
@@ -251,9 +467,10 @@ def get_clients(
     base_url: Any = '',
     timeout: float | None = None,
     follow_redirects: bool = True,
+    v2: bool = False,
 ) -> tuple[httpx.Client, httpx.AsyncClient]:
     base_url = str(base_url)
-    transport, atransport = _get_transports()
+    transport, atransport = _get_transports(v2)
     sync = httpx.Client(
         timeout=timeout,
         follow_redirects=follow_redirects,
